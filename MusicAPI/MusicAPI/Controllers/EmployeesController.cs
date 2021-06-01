e38515eff4a6116cebccd5c23e2a1fe95b7bdec2
Rudy Garcia <garciarud@kerndhs.com>
2021-06-01 09:22:04 -0700

    Initial project commit

---
+++ MusicAPI/MusicAPI/Controllers/EmployeesController.cs
@@ -0,0 +1,59 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using MusicAPI.Models;
+using MusicAPI.Data;
+
+namespace MusicAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class EmployeesController : ControllerBase
+    {
+        private readonly ApiDbContext _context; 
+
+        public EmployeesController(ApiDbContext context)
+        {
+            _context = context;
+        }
+
+        [HttpGet]
+        public IEnumerable<Personal> Get()
+        {
+            return _context.Personal.ToList();
+        }
+
+        [HttpGet("{id}")]
+        public Personal Get(string id)
+        {
+            return _context.Personal.Find(id);
+        }
+
+        [HttpPost]
+        public void Post([FromBody] Personal personal)
+        {
+            _context.Personal.Add(personal);
+            _context.SaveChanges();
+        }
+        
+        [HttpPut("{id}")]
+        public void Put(int id, [FromBody] Personal personal)
+        {
+            var person = _context.Personal.Find(id);
+            person.FirstName = personal.FirstName;
+            person.LastName = personal.LastName;
+            _context.SaveChanges();
+        }
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+            var person = _context.Personal.Find(id);
+            _context.Personal.Remove(person);
+            _context.SaveChanges();
+        }
+    }
+}
---
+++ MusicAPI/MusicAPI/Models/Personal.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace MusicAPI.Models
+{
+    [Table("Personal")]
+    public class Personal
+    {
+        [Key]
+        public string EmployeeId { get; set; }
+        public string FirstName { get; set; }
+        public string MiddleName { get; set; }
+        public string LastName { get; set; }
+
+        public string EmployeeName { get; set; }
+
+        public DateTime DateOfBirth { get; set; }
+
+        public char Gender { get; set; }
+
+        public int? PersonId { get; set; }
+    }
+}
