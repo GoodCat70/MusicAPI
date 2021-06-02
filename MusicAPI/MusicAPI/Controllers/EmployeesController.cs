31d6cd736cff728306238ff0a30c8452dc9aa516
Rudy Garcia <garciarud@kerndhs.com>
2021-06-02 09:45:54 -0700

    Added comments to all endpoints in controller

--- MusicAPI/MusicAPI/Controllers/EmployeesController.cs
+++ MusicAPI/MusicAPI/Controllers/EmployeesController.cs
@@ -25,16 +25,20 @@ namespace MusicAPI.Controllers
         public async Task<IActionResult> Get()
         {
             //IAction Result is a way of returning status codes along with data.
-            //async needs 
+            //Task<IActionResult> is used when you need to return some data. 
+            //Task would be used when you do not need to return data
+            //Async allows the rest of the application to run while this method executes
             return Ok( await _context.Personal.ToListAsync());
         }
 
+                //URL param = {id}
         [HttpGet("{id}")]
         public async Task<IActionResult> Get(string id)
         {
             var person = await _context.Personal.FindAsync(id);
             if(person == null)
             {
+                //You can return custom error messages in the NotFound Object
                 return NotFound("No Record Found"); 
             }
             else
@@ -43,7 +47,7 @@ namespace MusicAPI.Controllers
             }
         }
 
-        [HttpPost]
+        [HttpPost]                             //From Body tells the endpoint to accept data from the body of the request           
         public async Task<IActionResult> Post([FromBody] Personal personal)
         {
             if(personal != null)
@@ -58,8 +62,8 @@ namespace MusicAPI.Controllers
                 return NotFound("Record is not complete"); 
             }
         }
-        
-        [HttpPut("{id}")]
+            //URL param
+        [HttpPut("{id}")]                    //URL Param , From Body tells the endpoint to accept data from the body of the request
         public async Task<IActionResult> Put(int id, [FromBody] Personal personal)
         {
             var person = await _context.Personal.FindAsync(id);
@@ -72,6 +76,7 @@ namespace MusicAPI.Controllers
                 person.FirstName = personal.FirstName;
                 person.LastName = personal.LastName;
                 await _context.SaveChangesAsync();
+                //You can return a custom message in the OK object.
                 return Ok("Record Updated Successfully");
             }
         }
@@ -80,11 +85,12 @@ namespace MusicAPI.Controllers
         {
             var person = await _context.Personal.FindAsync(id);
             if(person == null)
-            {
+            {         
                 return NotFound("That record was not found"); 
             }
             else
             {
+                                    //There is no RemoveAsync. So you cannot await this operation
                 _context.Personal.Remove(person);
                 await _context.SaveChangesAsync();
                 return Ok("Record Deleted");
